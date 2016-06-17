6340c1f7ce120db863b7e902761c4d3df7829dda
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-06-17 17:37:01 +0200

    First and probably last commit

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Play () {
+		Application.LoadLevel (1);
+	}
+	
+	// Update is called once per frame
+	public void Quit () {
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/setCursor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setCursor : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+	}
+}
