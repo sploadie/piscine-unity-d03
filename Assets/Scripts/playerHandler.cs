8dc399b78ee226c0422b917119891015702f2ff7
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-06-17 23:25:42 +0200

    Finished stuff hopefully

---
+++ Assets/Scripts/playerHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerHandler : MonoBehaviour {
+
+	public static playerHandler instance { get; private set; }
+
+	public Text  energy_display;
+	public Text  hp_display;
+	public Text  score_display;
+
+	private turrentPanel turret;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		energy_display.text = "" + gameManager.gm.playerEnergy;
+		hp_display.text = "" + gameManager.gm.playerHp;
+		score_display.text = "" + gameManager.gm.score;
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+			if (hit.collider != null) {
+				Debug.Log ("Clicked on " + hit.collider.gameObject.tag);
+				turrentPanel new_turret = hit.collider.gameObject.GetComponent<turrentPanel> ();
+				if (turret == null && new_turret != null && new_turret.can_buy) {
+					turret = new_turret;
+				}
+			}
+		}
+		if (!Input.GetMouseButton (0) && turret) {
+			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+			if (hit.collider != null && hit.collider.gameObject.tag == "empty") {
+				gameManager.gm.playerEnergy -= turret.tower.energy;
+				GameObject.Instantiate (turret.tower, hit.collider.transform.position, hit.collider.transform.rotation);
+			}
+			turret.sprite.transform.position = turret.sprite_position;
+			turret = null;
+		}
+		if (turret) {
+			Vector3 mouse_position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			mouse_position.z = turret.sprite_position.z;
+			turret.sprite.transform.position = mouse_position;
+		}
+	}
+}
---
+++ Assets/Scripts/turrentPanel.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class turrentPanel : MonoBehaviour {
+
+	public towerScript tower;
+	public RectTransform sprite;
+	public Text energy;
+	public Text range;
+	public Text power;
+	public Image flying;
+	public Sprite not_flying;
+	public Sprite canon_sprite;
+	public Sprite rocket_sprite;
+	public Sprite gatling_sprite;
+
+	public Vector3 sprite_position { get; private set; }
+
+	public bool can_buy { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+		can_buy = true;
+		Debug.Log ("Size: " + GetComponent<RectTransform> ().rect.size);
+		GetComponent<BoxCollider2D> ().size = GetComponent<RectTransform> ().rect.size;
+		sprite_position = sprite.transform.position;
+		energy.text = "" + tower.energy;
+		range.text = "" + tower.range;
+		power.text = "" + tower.damage;
+		if (tower.type == towerScript.Type.canon) {
+			sprite.GetComponent<Image> ().sprite = canon_sprite;
+			flying.sprite = not_flying;
+		} else if (tower.type == towerScript.Type.rocket) {
+			sprite.GetComponent<Image> ().sprite = rocket_sprite;
+		} else {
+			sprite.GetComponent<Image> ().sprite = gatling_sprite;
+		}
+	}
+
+	void Update () {
+		if (gameManager.gm.playerEnergy < tower.energy) {
+			can_buy = false;
+			sprite.GetComponent<Image> ().color = Color.red;
+		} else {
+			can_buy = true;
+			sprite.GetComponent<Image> ().color = Color.white;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawIcon (transform.position, "death.png", true);
+	}
+}
